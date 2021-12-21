5e3b2c4eb9474750522a8fafb40b8161315dbccc
Hnin Yu <hninyuhlaing1520@gmail.com>
2021-12-21 13:51:32 +0630

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -80,6 +80,12 @@ end
 	)
 end
 
-
 puts "9 Portfolio created"
 
+3.times do |technology|
+	Portfolio.last.technologies.create!(
+		name: "Technology #{technology}"
+	)
+end
+
+puts "3 technologies created"
